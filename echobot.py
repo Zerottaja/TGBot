519822c2070cb2458da3c7dfa8f23c6f217e639e
Zerottaja <samu.ampio@student.tut.fi>
2017-03-24 22:25:25 +0200

    Lisätty whitelist

--- echobot.py
+++ echobot.py
@@ -3,6 +3,7 @@ import requests
 
 TOKEN = "308527009:AAFPg5p53k-I0iYuWJNU-eDJTRGutg2Xx_8"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+WHITELIST = {21942357, 152093174}
 
 
 def get_url(url):
@@ -26,15 +27,20 @@ def get_updates():
 def get_last_chat_id_and_text(updates):
     num_updates = len(updates["result"])
     last_update = num_updates - 1
-    text = updates["result"][last_update]["message"]["text"]+" :--DD"
+    text = updates["result"][last_update]["message"]["text"]
     chat_id = updates["result"][last_update]["message"]["chat"]["id"]
-    return (text, chat_id)
+    return text, chat_id
 
 
 def send_message(text, chat_id):
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
 
-
-text, chat = get_last_chat_id_and_text(get_updates())
-send_message(text, chat)
+paivitykset = get_updates()
+viime_paiv = len(paivitykset["result"]) - 1
+if paivitykset["result"][viime_paiv]["message"]["from"]["id"] in WHITELIST:
+    teksti, chat = get_last_chat_id_and_text(paivitykset)
+    send_message(teksti, chat)
+else:
+    chat = get_last_chat_id_and_text(paivitykset)[1]
+    send_message("Who are you?", chat)
