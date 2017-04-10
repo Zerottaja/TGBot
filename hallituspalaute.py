c0e663ac31d9ca67d1d8e741a6275b2c385974e8
Samu.Ampio <samu.ampio@student.tut.fi>
2017-04-10 19:56:12 +0300

    Hallituspalautteen raportointi toimii. Lisätty myös automaattinen palautelokin siistintä

--- hallituspalaute.py
+++ hallituspalaute.py
@@ -7,12 +7,21 @@ def kirjaa_hallituspalaute(palaute):
     wb = load_workbook('hallituspalaute.xlsx')
     worksheet = wb.active
     palautteiden_lkm = worksheet.max_row
-    kirjattava = "{} {}.{}.{} @ {}:{}".format(palaute, time.localtime()[2],
-                                              time.localtime()[1],
-                                              time.localtime()[0],
-                                              time.localtime()[3],
-                                              time.localtime()[4])
+
+    kirjattava = "{} @ {}.{}.{}, {}:{}".format(palaute, time.localtime()[2],
+                                               time.localtime()[1],
+                                               time.localtime()[0],
+                                               time.localtime()[3],
+                                               time.localtime()[4])
     worksheet['A{}'.format(palautteiden_lkm + 1)] = kirjattava
+    palautteiden_lkm += 1
+
+    if palautteiden_lkm >= 101:
+        print("Palautteiden maksimimaara ylitetty, siistitaan lokia..")
+        for rivi in range(52, palautteiden_lkm + 1):
+            worksheet['A{}'.format(rivi-50)] = \
+                worksheet['A{}'.format(rivi)].value
+            worksheet['A{}'.format(rivi)].value = None
 
     try:
         wb.save('hallituspalaute.xlsx')
@@ -27,12 +36,13 @@ def raportti():
     wb = load_workbook('hallituspalaute.xlsx')
     worksheet = wb.active
     palautteiden_lkm = worksheet.max_row
-    if palautteiden_lkm <= 6:
+    if palautteiden_lkm <= 11:
         alaraja = 2
     else:
-        alaraja = palautteiden_lkm - 5
-    palautettava = ""
+        alaraja = palautteiden_lkm - 9
+    palautettava = "10 viimeisintä palautetta:\n"
     for i in range(alaraja, palautteiden_lkm + 1):
-        palautettava = palautettava + worksheet['A{}'.format(i)].value + "\n"
+        palautettava = palautettava + str(worksheet['A{}'
+                                          .format(i)].value) + "\n"
 
     return palautettava
